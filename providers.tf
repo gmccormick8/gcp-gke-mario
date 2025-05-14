a1ade1f3d0c088d09f97419a1635ebc8f448351e
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-14 10:17:09 -0700

    version adjustments

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "~> 1.11.0"
+  required_version = "~> 1.11"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.30.0"
+      version = "~> 6.30"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.30.0"
+      version = "~> 2.30"
     }
   }
 
