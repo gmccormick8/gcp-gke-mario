96c9ab257861849b31d0d7e8708f796d1e2613a0
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-16 09:52:46 -0700

    bug fixes

--- main.tf
+++ main.tf
@@ -50,8 +50,8 @@ module "cluster-central" {
   region                 = "us-central1"
   network_name           = module.prod-vpc.network_self_link
   subnet_name            = module.prod-vpc.subnets["prod-central-vpc"].self_link
-  pods_cidr              = module.prod-vpc.subnets["prod-central-vpc"].subnets["prod-central-pods"].self_link
-  services_cidr          = module.prod-vpc.subnets["prod-central-vpc"].subnets["prod-central-services"].self_link
+  pods_cidr              = module.prod-vpc.secondary_ranges["prod-central-vpc"][0].ip_cidr_range
+  services_cidr          = module.prod-vpc.secondary_ranges["prod-central-vpc"][1].ip_cidr_range
   master_ipv4_cidr_block = "172.16.0.0/28"
 }
 
