3869a3119eed5ec1f8affc80fecb46bdb912274b
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-16 15:47:40 -0700

    bug fix

--- modules/k8s/main.tf
+++ modules/k8s/main.tf
@@ -1,7 +1,7 @@
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host                   = "http://${var.cluster_endpoint}"
+  host                   = "https://${var.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
