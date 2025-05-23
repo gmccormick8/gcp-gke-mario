a31265bbf690879f33385f2ad6a2a2088847de6a
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-23 16:10:06 -0700

    bug fixes

--- modules/k8s/main.tf
+++ modules/k8s/main.tf
@@ -15,8 +15,8 @@ provider "helm" {
 }
 
 # Deploy Mario to cluster
-resource "helm_release" "mario" {
-  name             = "mario"
+resource "helm_release" "mario2" {
+  name             = "mario2"
   chart            = "${path.module}/helm/mario"
   namespace        = "mario"
   create_namespace = true
