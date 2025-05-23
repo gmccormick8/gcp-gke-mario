5ae99d8d6d8c2a8094d96771bba969b997f6006c
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-23 11:18:03 -0700

    bug fixes

--- modules/k8s/outputs.tf
+++ modules/k8s/outputs.tf
@@ -2,12 +2,3 @@ output "load_balancer_ip" {
   description = "The IP address of the load balancer"
   value       = data.kubernetes_service.gateway_ip[0].status[0].load_balancer[0].ingress[0].ip
 }
-
-data "kubernetes_service" "gateway_ip" {
-  count = var.extra_values.gateway.enabled ? 1 : 0
-  metadata {
-    name      = "external-http"
-    namespace = helm_release.mario.namespace
-  }
-  depends_on = [helm_release.mario]
-}
