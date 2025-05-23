ff602d1e6c10a4886baad096e31c870317051868
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-23 11:19:00 -0700

    bug fix

--- modules/k8s/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "load_balancer_ip" {
-  description = "The IP address of the load balancer"
-  value       = data.kubernetes_service.gateway_ip[0].status[0].load_balancer[0].ingress[0].ip
-}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "mario_endpoint" {
-  description = "The endpoint URL where the Mario game will be accessible"
-  value       = "http://${module.k8s-mario-central.load_balancer_ip}"
-}
