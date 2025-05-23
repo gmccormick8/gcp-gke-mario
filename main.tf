6fa8264464f5e6def0fbfb607aace0743257f146
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-23 14:23:11 -0700

    bug fix

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "google_gke_hub_feature" "mci" {
 
   spec {
     multiclusteringress {
-      config_membership = "projects/${var.project_id}/locations/global/memberships/${module.prod-central-cluster.name}"
+      config_membership = "projects/${var.project_id}/locations/global/memberships/${module.prod-central-cluster.cluster_name}"
     }
   }
 }
