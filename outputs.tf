8c9d9c029e011b23e2205f85cf6f21c49ea9d2d7
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-23 10:07:20 -0700

    Remove Mario endpoint output from Terraform configuration

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "mario_endpoint" {
-  description = "The external IP address where the Mario game can be accessed"
-  value       = data.kubernetes_service_v1.gateway_service.status[0].load_balancer[0].ingress[0].ip
-}
