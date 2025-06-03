ea502a8634b8960e624c477554b09f7635cc6762
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-03 11:07:27 -0700

    Fix mario_endpoint output to handle null load balancer IP values

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "mario_endpoint" {
   description = "You can access the Mario game at this URL"
-  value       = "http://${module.k8s-mario-central.load_balancer_ip}"
+  value       = try(coalesce("http://${module.k8s-mario-central.load_balancer_ip}", "Waiting for load balancer IP... Please try again in a few minutes."), "Waiting for load balancer IP... Please try again in a few minutes.")
 }
