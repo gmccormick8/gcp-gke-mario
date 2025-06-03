1a8beb1c7a59591b9369b4d04ed453ba8ffe1430
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-03 09:56:42 -0700

    Refactor cluster and deployment configurations for improved scalability and maintainability

--- main.tf
+++ main.tf
@@ -1,3 +1,44 @@
+locals {
+  clusters = {
+    east = {
+      name             = "east-cluster"
+      region           = "us-east5"
+      master_cidr      = "172.16.0.0/28"
+      subnet_key       = "prod-east-vpc"
+      pods_network     = "prod-east-pods"
+      services_network = "prod-east-services"
+    }
+    central = {
+      name             = "central-cluster"
+      region           = "us-central1"
+      master_cidr      = "172.16.1.0/28"
+      subnet_key       = "prod-central-vpc"
+      pods_network     = "prod-central-pods"
+      services_network = "prod-central-services"
+    }
+    west = {
+      name             = "west-cluster"
+      region           = "us-west4"
+      master_cidr      = "172.16.2.0/28"
+      subnet_key       = "prod-west-vpc"
+      pods_network     = "prod-west-pods"
+      services_network = "prod-west-services"
+    }
+  }
+
+  k8s_deployments = {
+    east = {
+      config_cluster = false
+    }
+    central = {
+      config_cluster = true
+    }
+    west = {
+      config_cluster = false
+    }
+  }
+}
+
 # Create a VPC network and subnet
 module "prod-vpc" {
   source       = "./modules/network"
@@ -46,52 +87,17 @@ module "prod-vpc" {
   cloud_nat_configs = ["us-east5", "us-central1", "us-west4"]
 }
 
-module "prod-east-cluster" {
+module "prod-clusters" {
+  for_each               = local.clusters
   source                 = "./modules/gke"
   project_id             = var.project_id
-  cluster_name           = "east-cluster"
-  zone                   = "${module.prod-vpc.subnets["prod-east-vpc"].region}-c"
+  cluster_name           = each.value.name
+  zone                   = "${each.value.region}-c"
   network_name           = module.prod-vpc.network_self_link
-  subnet_name            = module.prod-vpc.subnets["prod-east-vpc"].self_link
-  pods_network_name      = "prod-east-pods"
-  services_network_name  = "prod-east-services"
-  master_ipv4_cidr_block = "172.16.0.0/28"
-  public_ip              = var.public_ip
-  min_node_count         = 1
-  max_node_count         = 3
-  machine_type           = "e2-small"
-  disk_size_gb           = 25
-  disk_type              = "pd-standard"
-}
-
-module "prod-central-cluster" {
-  source                 = "./modules/gke"
-  project_id             = var.project_id
-  cluster_name           = "central-cluster"
-  zone                   = "${module.prod-vpc.subnets["prod-central-vpc"].region}-c"
-  network_name           = module.prod-vpc.network_self_link
-  subnet_name            = module.prod-vpc.subnets["prod-central-vpc"].self_link
-  pods_network_name      = "prod-central-pods"
-  services_network_name  = "prod-central-services"
-  master_ipv4_cidr_block = "172.16.1.0/28"
-  public_ip              = var.public_ip
-  min_node_count         = 1
-  max_node_count         = 3
-  machine_type           = "e2-small"
-  disk_size_gb           = 25
-  disk_type              = "pd-standard"
-}
-
-module "prod-west-cluster" {
-  source                 = "./modules/gke"
-  project_id             = var.project_id
-  cluster_name           = "west-cluster"
-  zone                   = "${module.prod-vpc.subnets["prod-west-vpc"].region}-c"
-  network_name           = module.prod-vpc.network_self_link
-  subnet_name            = module.prod-vpc.subnets["prod-west-vpc"].self_link
-  pods_network_name      = "prod-west-pods"
-  services_network_name  = "prod-west-services"
-  master_ipv4_cidr_block = "172.16.2.0/28"
+  subnet_name            = module.prod-vpc.subnets[each.value.subnet_key].self_link
+  pods_network_name      = each.value.pods_network
+  services_network_name  = each.value.services_network
+  master_ipv4_cidr_block = each.value.master_cidr
   public_ip              = var.public_ip
   min_node_count         = 1
   max_node_count         = 3
@@ -104,6 +110,12 @@ resource "google_gke_hub_feature" "mcs" {
   name     = "multiclusterservicediscovery"
   project  = var.project_id
   location = "global"
+
+  depends_on = [
+    module.prod-clusters["east"],
+    module.prod-clusters["central"],
+    module.prod-clusters["west"]
+  ]
 }
 
 resource "google_gke_hub_feature" "mci" {
@@ -113,46 +125,35 @@ resource "google_gke_hub_feature" "mci" {
 
   spec {
     multiclusteringress {
-      config_membership = "projects/${var.project_id}/locations/us-central1/memberships/${module.prod-central-cluster.cluster_name}"
+      config_membership = "projects/${var.project_id}/locations/us-central1/memberships/${module.prod-clusters["central"].cluster_name}"
     }
   }
+
+  depends_on = [
+    google_gke_hub_feature.mcs,
+    module.prod-clusters["east"],
+    module.prod-clusters["central"],
+    module.prod-clusters["west"]
+  ]
 }
 
-module "k8s-mario-east" {
+module "k8s-mario" {
+  for_each         = local.k8s_deployments
   source           = "./modules/k8s"
   project_id       = var.project_id
-  cluster_name     = module.prod-east-cluster.cluster_name
-  cluster_location = module.prod-east-cluster.cluster_location
-  cluster_endpoint = module.prod-east-cluster.cluster_endpoint
-  cluster_ca_cert  = module.prod-east-cluster.master_auth.cluster_ca_certificate
+  cluster_name     = module.prod-clusters[each.key].cluster_name
+  cluster_location = module.prod-clusters[each.key].cluster_location
+  cluster_endpoint = module.prod-clusters[each.key].cluster_endpoint
+  cluster_ca_cert  = module.prod-clusters[each.key].master_auth.cluster_ca_certificate
   min_replicas     = 1
   max_replicas     = 5
   image            = "sevenajay/mario:latest"
-  config_cluster   = false
-}
+  config_cluster   = each.value.config_cluster
 
-module "k8s-mario-central" {
-  source           = "./modules/k8s"
-  project_id       = var.project_id
-  cluster_name     = module.prod-central-cluster.cluster_name
-  cluster_location = module.prod-central-cluster.cluster_location
-  cluster_endpoint = module.prod-central-cluster.cluster_endpoint
-  cluster_ca_cert  = module.prod-central-cluster.master_auth.cluster_ca_certificate
-  min_replicas     = 1
-  max_replicas     = 5
-  image            = "sevenajay/mario:latest"
-  config_cluster   = true
-}
-
-module "k8s-mario-west" {
-  source           = "./modules/k8s"
-  project_id       = var.project_id
-  cluster_name     = module.prod-west-cluster.cluster_name
-  cluster_location = module.prod-west-cluster.cluster_location
-  cluster_endpoint = module.prod-west-cluster.cluster_endpoint
-  cluster_ca_cert  = module.prod-west-cluster.master_auth.cluster_ca_certificate
-  min_replicas     = 1
-  max_replicas     = 5
-  image            = "sevenajay/mario:latest"
-  config_cluster   = false
+  depends_on = [
+    google_gke_hub_feature.mci,
+    module.prod-clusters["east"],
+    module.prod-clusters["central"],
+    module.prod-clusters["west"]
+  ]
 }
