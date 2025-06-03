d9fdd1b492bf4bfe2917e0baec44cea7d0c2c77d
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-03 11:03:59 -0700

    bug fixes

--- modules/k8s/outputs.tf
+++ modules/k8s/outputs.tf
@@ -12,5 +12,5 @@ data "kubernetes_service_v1" "mario_lb" {
 
 output "load_balancer_ip" {
   description = "The IP address of the global load balancer"
-  value       = data.kubernetes_service_v1.mario_lb.object.status.addresses[0].value
+  value       = data.kubernetes_service_v1.mario_lb.status[0].load_balancer[0].ingress[0].ip
 }
