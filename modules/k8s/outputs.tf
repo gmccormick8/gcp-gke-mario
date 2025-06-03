bc2a3ead79ce06f43aa73b35310550870f5497aa
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-03 11:04:48 -0700

    Fix load balancer IP output to handle potential null values

--- modules/k8s/outputs.tf
+++ modules/k8s/outputs.tf
@@ -1,5 +1,4 @@
 data "kubernetes_service_v1" "mario_lb" {
-
   metadata {
     name      = "mario-external-gateway"
     namespace = "mario"
@@ -12,5 +11,5 @@ data "kubernetes_service_v1" "mario_lb" {
 
 output "load_balancer_ip" {
   description = "The IP address of the global load balancer"
-  value       = data.kubernetes_service_v1.mario_lb.status[0].load_balancer[0].ingress[0].ip
+  value       = try(data.kubernetes_service_v1.mario_lb.status.0.load_balancer.0.ingress.0.ip, null)
 }
