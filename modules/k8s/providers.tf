40dbda46bac21aa32124d81ee1503623ee78c7e8
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-03 10:15:25 -0700

    Add provider configurations for GKE and K8s modules

---
+++ modules/k8s/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.11"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.30"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
+  }
+}
