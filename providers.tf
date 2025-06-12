9724d67773a839ef9a8cea6f53ab990b83ce8271
Gabriel McCormick <74574750+gmccormick8@users.noreply.github.com>
2025-06-12 16:33:46 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,23 +4,19 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.36"
+      version = "~> 6.30"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.36"
+      version = "~> 6.30"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = "~> 2.30"
     }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.10"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.10"
     }
   }
 
